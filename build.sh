20707d52d0616dcb4609a5bc313bb527a59b240f
JimLee1996 <59061463@qq.com>
2019-07-15 16:14:15 +0800

    linux only

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if hash upx 2>/dev/null; then
 	UPX=true
 fi
 
-VERSION=1.0
+VERSION=FakeTCP
 LDFLAGS="-X main.VERSION=$VERSION -s -w"
 GCFLAGS=""
 
@@ -16,32 +16,12 @@ fi
 cd tun
 rm -rf *
 
-
-
-# Server
+# x86_64
 os=linux
 arch=amd64
 env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_${os}_${arch}${suffix} github.com/JimLee1996/tun/server
+env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_${os}_${arch}${suffix} github.com/JimLee1996/tun/client
 if $UPX; then upx -9 client_${os}_${arch}${suffix} server_${os}_${arch}${suffix};fi
-
-os=darwin
-arch=amd64
-env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_${os}_${arch}${suffix} github.com/JimLee1996/tun/server
-if $UPX; then upx -9 client_${os}_${arch}${suffix} server_${os}_${arch}${suffix};fi
-
-
-# Client
-OSES=(darwin windows)
-arch=amd64
-for os in ${OSES[@]}; do
-	suffix=""
-	if [ "$os" == "windows" ]
-	then
-		suffix=".exe"
-	fi
-	env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_${os}_${arch}${suffix} github.com/JimLee1996/tun/client
-	if $UPX; then upx -9 client_${os}_${arch}${suffix} server_${os}_${arch}${suffix};fi
-done
 
 # MIPS32LE
 env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_linux_mipsle github.com/JimLee1996/tun/client
